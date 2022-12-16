5c4bf59eb7aa05e06b3e0d50c0198a50305f3905
Alexandre <alexandre.michelix@dotic.fr>
2022-12-16 11:16:40 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-<<<<<<< develop
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./app/pages/Home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const Routing = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" />
-          {/*<Route path="/login" element={<Login />} />*/}
-          {/*<Route path="/register" element={<Register />} />*/}
-          {/*<Route path="/profile/:id" element={<Profile />} />*/}
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-=======
-import React from 'react';
-import './App.css';
-import Home from "./app/pages/Home/Home";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const Routing = () => {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Home/>}/>
                     {/*<Route path="/login" element={<Login />} />*/}
                     {/*<Route path="/register" element={<Register />} />*/}
                     {/*<Route path="/profile/:id" element={<Profile />} />*/}
@@ -35,21 +17,16 @@ const Routing = () => {
             </BrowserRouter>
         </div>
     );
->>>>>>> init pages
-};
+}
 
 function App() {
   return (
     <div className="App">
-<<<<<<< develop
       {/*<header className="App-header"></header>*/}
       <body>
         <Navbar></Navbar>
         <Routing></Routing>
       </body>
-=======
-        <Routing></Routing>
->>>>>>> init pages
     </div>
   );
 }
